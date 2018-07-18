1630472e9f6be5718b48716644ac5986a6d1a9c1
Vladimir Semenovich <37423497+developer-vs@users.noreply.github.com>
2018-07-17 19:49:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 define(function (require, exports, module) {
-    "use strict";
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        EditorManager = brackets.getModule("editor/EditorManager"),
-        KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
-        Menus = brackets.getModule("command/Menus");
 
+    // package-style naming to avoid collisions
+    var COMMAND_ID = "brackets_html_boilerplate.developer-vs";
+    
+    var commandManager = brackets.getModule("command/CommandManager");
+    var editorManager = brackets.getModule("editor/EditorManager");
+    var keyBindingManager = brackets.getModule("command/KeyBindingManager");
+    
+    // Menu
+    var menus = brackets.getModule("command/Menus");
+
+    var menu = menus.getMenu(menus.AppMenuBar.EDIT_MENU); 
+    menu.addMenuDivider();
+    menu.addMenuItem(COMMAND_ID);
+    menu.addMenuDivider();
+
+    // var contextMenu  = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+    // contextMenu.addMenuItem(COMMAND_ID);
+  
     function htmlTemplate() {
-        var editor = EditorManager.getFocusedEditor();
+
+        var editor = editorManager.getFocusedEditor();
+
         if (editor) {
             var template = "<!DOCTYPE html>\n" +
                 "<html>\n" +
@@ -16,15 +31,17 @@ define(function (require, exports, module) {
                 "<body>\n\n" +
                 "</body>\n" +
                 "</html>";
+
             var inPos = editor.getCursorPos();
             editor.document.replaceRange(template, inPos);
         }
     };
-    var COMMAND_ID = "brackets_html_boilerplate.developer-vs"; // package-style naming to avoid collisions
-    CommandManager.register("HTML Boilerplate", COMMAND_ID, htmlTemplate);
-    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
-    menu.addMenuDivider();
-    menu.addMenuItem(COMMAND_ID);
-   KeyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "mac");
-   KeyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "win");
+     
+    commandManager.register("HTML Boilerplate", COMMAND_ID, htmlTemplate);
+
+    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    keyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "mac");
+    keyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "win"); 
+    keyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "linux");
+    
 });
