32f51ea99f583bd5fed2252e5c74318355c4dfe5
webdev-vs <vladimirgdsr@gmail.com>
2018-07-18 18:52:14 -0700

    added toolbar button

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 define(function (require, exports, module) {
 
+    'use strict';   
+
     // package-style naming to avoid collisions
     var COMMAND_ID = "brackets_html_boilerplate.developer-vs";
     
-    var CommandManager = brackets.getModule("command/CommandManager");
-    var EditorManager = brackets.getModule("editor/EditorManager");
-    var KeyBindingManager = brackets.getModule("command/KeyBindingManager");
+    var ExtensionUtils    = brackets.getModule('utils/ExtensionUtils');
+    var CommandManager    = brackets.getModule("command/CommandManager");
+    var EditorManager     = brackets.getModule("editor/EditorManager");
     
-    // Menu
-    var Menus = brackets.getModule("command/Menus");
-    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU); 
-    var contextMenu  = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);    
-  
+
     function htmlTemplate() {
 
         var editor = EditorManager.getFocusedEditor();
@@ -26,18 +24,51 @@ define(function (require, exports, module) {
                 "</body>\n" +
                 "</html>";
 
-            editor.document.replaceRange(template, editor.getCursorPos());
+            try {
+                editor.document.replaceRange(template, editor.getCursorPos());
+            } catch (err) {}
         }
     };
      
     CommandManager.register("HTML Boilerplate", COMMAND_ID, htmlTemplate);
-    menu.addMenuDivider();
-    menu.addMenuItem(COMMAND_ID);
-    contextMenu.addMenuItem(COMMAND_ID);
 
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-    KeyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "mac");
-    KeyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "win"); 
-    KeyBindingManager.addBinding(COMMAND_ID, "Ctrl-Alt-I", "linux");
+    // Menu
+    var Menus       = brackets.getModule("command/Menus");
+    var editMenu    = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU); 
+    var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);    
+
+    var KEY_BINDINGS = [
+        {
+            key: 'Ctrl-Alt-I',
+            platform: 'win'
+        }, {
+            key: 'Cmd-Alt-I',
+            platform: 'mac'
+        }, {
+            key: 'Ctrl-Alt-I',
+            platform: 'linux'
+        }
+    ];  
+
+    // Main Menu
+    editMenu.addMenuDivider();
+    editMenu.addMenuItem(COMMAND_ID, KEY_BINDINGS);
+    editMenu.addMenuDivider();
+
+    // Context Menu
+    contextMenu.addMenuDivider();
+    contextMenu.addMenuItem(COMMAND_ID);
+    contextMenu.addMenuDivider();
+
+    // Toolbar Button
+    ExtensionUtils.loadStyleSheet(module, 'styles/styles.css');
+    $(document.createElement('a'))
+        .attr('id', 'html-boilerplate-toolbar-icon')
+        .attr('href', '#')
+        .attr('title', 'HTML Boilerplate')
+        .on('click', function () {
+            htmlTemplate();
+        })
+        .appendTo($('#main-toolbar .buttons'));
     
 });
