d297270e229f22cf18137a04bc1161785081ca13
Vladimir Semenovich <37423497+developer-vs@users.noreply.github.com>
2018-07-25 16:39:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,134 +3,101 @@ define(function (require, exports, module) {
     'use strict';
 
     // Package-style naming to avoid collisions
-    var COMMAND_ID        = 'brackets_html_boilerplate.expandWithTab';
+    var COMMAND_ID = 'brackets_html_boilerplate.expandWithTab';
 
-    var ExtensionUtils    = brackets.getModule('utils/ExtensionUtils');
-    var CommandManager    = brackets.getModule('command/CommandManager');
-    var EditorManager     = brackets.getModule('editor/EditorManager');
-    var DocumentManager   = brackets.getModule('document/DocumentManager');
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
+    var CommandManager = brackets.getModule('command/CommandManager');
+    var EditorManager = brackets.getModule('editor/EditorManager');
+    var DocumentManager = brackets.getModule('document/DocumentManager');
 
     // It will allows to use "Tab" key but not showing up the keys in the Menu
     var KeyBindingManager = brackets.getModule('command/KeyBindingManager');
 
     // HTML 5 template
-    var template          = require('text!templates/html5.html');
+    var htmlTemplate = require('text!templates/html5.html');
+    var spaces = '    ';
 
-    function replaceSelectionOnSpaces() {
-      var editor = EditorManager.getFocusedEditor();
 
-      try {
-          var selection = editor.getSelection();
-          editor.document.replaceRange('    ', selection.start, selection.end);
-      } catch (err) {}
-    }
-
-    function replaceSelectionForEmptyLine() {
-
-        var editor = EditorManager.getFocusedEditor();
-
+    // Will insert spaces where the document type is not HTML or where 'Tab' key not active
+    // or will replace selection on the HTML template
+    function insertTemplate(editor, replacement) {
         try {
-            var selection = editor.getSelection();
-            editor.document.replaceRange(template, selection.start, selection.end);
-        } catch (err) {}
-    }
 
-
-    function replaceSelection(editor) {
-        try {
             var selection = editor.getSelection();
-            editor.document.replaceRange(template, selection.start, selection.end);
-        } catch (err) {}
+            editor.document.replaceRange(replacement, selection.start, selection.end);
+        } catch (err) {
+            // do nothing
+        }
     }
 
 
     function expandWithTab() {
+
+        // Returns the Document that is currently open in the editor UI. May be null.
         var document = DocumentManager.getCurrentDocument();
 
+        // Will only return editors that currently have focus
+        var editor = EditorManager.getFocusedEditor();
+
         if (document.getLanguage().getId() === 'html') {
-            var editor = EditorManager.getFocusedEditor();
 
             // True if there's a text selection; false if not
             if (editor.hasSelection()) {
+
                 // Expand abbreviation if there’s a selection
-                replaceSelection(editor);
+                insertTemplate(editor, htmlTemplate);
+
             } else {
-                // Get text from line
+
                 var currentCursorPosition = editor.getCursorPos();
-                var line = editor.document.getLine(currentCursorPosition.line);
-                var line = line.trim();
+
+                // Get text from line
+                var line = (editor.document.getLine(currentCursorPosition.line)).trim();
 
                 // Handle Tab key for known syntaxes only
                 if (line === 'html' || line === '<html' || line === 'html>' || line === '<html>') {
-                    var beginningOfLine = {line: currentCursorPosition.line, ch: 0};
-                    var textBeforeCursor = editor.document.getRange(beginningOfLine, currentCursorPosition);
 
-                    //if (textBeforeCursor.trim() === '') {
-                        // Do nothing if current cursor position before text
-                    //} else {
-                    try {
-                        editor.document.replaceRange(template, beginningOfLine, currentCursorPosition);
-                    } catch (err) {}
-                } else {
-                    replaceSelectionOnSpaces();
-                }
-            }
-        } else {
-            // If document type not an HTML
-            replaceSelectionOnSpaces();
-        }
-    }
-
-    function expandWithTabForMenuIcon() {
-        var document = DocumentManager.getCurrentDocument();
-
-        if (document.getLanguage().getId() === 'html') {
-            var editor = EditorManager.getFocusedEditor();
-
-            // True if there's a text selection; false if not
-            if (editor.hasSelection()) {
-                // Expand abbreviation if there’s a selection
-                replaceSelection(editor);
-            } else {
-                // Get text from line
-                var currentCursorPosition = editor.getCursorPos();
-                var line = editor.document.getLine(currentCursorPosition.line);
-                var line = line.trim();
-
-                // Handle Tab key for known syntaxes only
-                if (line === 'html' || line === '<html' || line === 'html>' || line === '<html>') {
                     var beginningOfLine = {line: currentCursorPosition.line, ch: 0};
                     var textBeforeCursor = editor.document.getRange(beginningOfLine, currentCursorPosition);
 
                     if (textBeforeCursor.trim() === '') {
-                        // Do nothing if current cursor position before text
+
+                        insertTemplate(editor, spaces);
 
                     } else {
+
                         try {
-                            editor.document.replaceRange(template, beginningOfLine, currentCursorPosition);
-                        } catch (err) {}
+
+                            editor.document.replaceRange(htmlTemplate, beginningOfLine, currentCursorPosition);
+
+                        } catch (err) {
+                            // do nothing
+                        }
                     }
-                }
-                else {
-                    replaceSelection(editor);
+                } else {
+
+                    // If document type not an HTML
+                    insertTemplate(editor, spaces);
                 }
             }
         } else {
+
             // If document type not an HTML
+            insertTemplate(editor, spaces);
         }
     }
 
     CommandManager.register('HTML Boilerplate', COMMAND_ID, expandWithTab);
 
     // Menu
-    var Menus        = brackets.getModule('command/Menus');
-    var editMenu     = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
-    var contextMenu  = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+    var Menus = brackets.getModule('command/Menus');
+    var editMenu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
 
     var KEY_BINDINGS = [
-        { key: 'Tab', platform: 'win'   },
-        { key: 'Tab', platform: 'mac'   },
-        { key: 'Tab', platform: 'linux' }];
+        {key: 'Tab', platform: 'win'},
+        {key: 'Tab', platform: 'mac'},
+        {key: 'Tab', platform: 'linux'}];
 
     // It will allow to use "Tab" key but not showing up the keys in the Menu
     KeyBindingManager.addBinding(COMMAND_ID, KEY_BINDINGS);
@@ -151,7 +118,7 @@ define(function (require, exports, module) {
         .attr('href', '#')
         .attr('title', 'HTML Boilerplate')
         .on('click', function () {
-            expandWithTabForMenuIcon();
+            expandWithTab();
         })
         .appendTo($('#main-toolbar .buttons'));
 });
